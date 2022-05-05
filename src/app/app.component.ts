26268a46f5b9bf8dcbaaf400f6093474e1d72d6a
Ikvaratskhelia <ikvaratskhelia@crocobet.com>
2022-05-05 17:24:29 +0400

    added auto change of overflow on production

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {environment} from "../environments/environment";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
+  overflow: string = 'hidden';
+
+
+  ngOnInit() {
+    environment.production ? this.overflow = 'hidden' : this.overflow = 'visible';
+    document.body.style.overflow = this.overflow;
+    let currentHeight = 0;
+
+    document.body.style.minHeight = "unset";
+
+    setInterval(() => {
+      if (document.body.offsetHeight !== currentHeight) {
+        currentHeight = document.body.offsetHeight;
+      }
+    }, 250)
+
+  }
 }
