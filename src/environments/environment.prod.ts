9954e0b52a9e0aedebd89d395d3cdbc51961abaf
Nika Kopadze <Nkopadze@crocobet.com>
2022-06-13 14:15:26 +0400

    add rules

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
   cmsApi: 'https://cms.crocobet.com',
+  rulesApi: (lang: string) =>
+    `https://cms.crocobet.com/ui/policy/${lang}/categories/promotions/`,
   testToken: '',
 };
