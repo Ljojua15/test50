05b0b80d5718136015aa0b160c94d989a14e08ad
lgelashvili <lgelashvili@crocobet.com>
2022-07-06 16:59:58 +0400

    commit

--- src/app/services/campaign.service.ts
+++ src/app/services/campaign.service.ts
@@ -1,22 +1,43 @@
 import { Injectable } from '@angular/core';
-import {environment} from "../../environments/environment";
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
-import {GenericResponse} from "../models/response";
-import {User} from "../models/user";
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { GenericResponse } from '../models/response';
+import { User } from '../models/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CampaignService {
   readonly API = environment.cmsApi;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getUserData(campaignId: string): Observable<GenericResponse<User>> {
     return this.http.get<GenericResponse<User>>(
-      `${this.API}${campaignId}`
-    )
+      `${this.API}/campaigns/${campaignId}`
+    );
   }
 
+  getFinalData(campaignId: string): Observable<GenericResponse<User>> {
+    return this.http.get<GenericResponse<User>>(
+      `${this.API}/campaigns/${campaignId}`
+    );
+  }
+
+  getSchedule(eventId: string): Observable<any> {
+    return this.http.get(`https://cms.crocobet.com/twitch/get/${eventId}`);
+  }
+
+  getLiveStreams(): Observable<any> {
+    return this.http.get(`https://cms.crocobet.com/twitch?category=test-live`);
+  }
+
+  getButtonStatus(url: string) {
+    return this.http.get(url);
+  }
+
+  getPrize(campaignId: string): Observable<any> {
+    return this.http.post<any>(`${this.API}${campaignId}`, {});
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,10 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  rulesApi: (lang: string) =>
+    `https://cms.crocobet.com/ui/policy/${lang}/categories/promotions/`,
   production: false,
-  cmsApi: 'https://cms.crocobet.com',
-  testToken: ''
- };
+  cmsApi: 'https://cms.crocobet.com/',
+  testToken: '2b9d90e6-8cea-494b-b4d9-68f4ac6fee64.oddsapi1',
+  filePath: '',
+};
 
 /*
  * For easier debugging in development mode, you can import the following file
