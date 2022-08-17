0f2f5e779150dbf93e1ce621ff351483bba6d0ac
lgelashvili <lgelashvili@crocobet.com>
2022-08-17 18:58:46 +0400

    register and logic button update

--- src/app/ui/authorization-block/authorization-block.component.ts
+++ src/app/ui/authorization-block/authorization-block.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { IframeService } from 'src/app/services/iframe.service';
 
 @Component({
   selector: 'crc-authorization-block',
   templateUrl: './authorization-block.component.html',
-  styleUrls: ['./authorization-block.component.scss']
+  styleUrls: ['./authorization-block.component.scss'],
 })
 export class AuthorizationBlockComponent implements OnInit {
+  constructor(private iframeService: IframeService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onRegister() {
+    this.iframeService.register();
   }
 
+  onLogin() {
+    this.iframeService.login();
+  }
 }
