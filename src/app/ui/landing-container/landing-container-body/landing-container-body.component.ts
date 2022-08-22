5d4edae4aaf89291edda310715df661da11029f6
lgelashvili <lgelashvili@crocobet.com>
2022-08-22 11:52:36 +0400

    disable wheel button from body component

--- src/app/ui/landing-container/landing-container-body/landing-container-body.component.ts
+++ src/app/ui/landing-container/landing-container-body/landing-container-body.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'crc-landing-container-body',
   templateUrl: './landing-container-body.component.html',
-  styleUrls: ['./landing-container-body.component.scss']
+  styleUrls: ['./landing-container-body.component.scss'],
 })
 export class LandingContainerBodyComponent implements OnInit {
+  isDisabled = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
