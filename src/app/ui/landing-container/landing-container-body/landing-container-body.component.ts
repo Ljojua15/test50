6bd37efe5c8848dbac8477be8c12a7bd621e6b82
lgelashvili <lgelashvili@crocobet.com>
2022-08-25 14:38:55 +0400

    get user data for progress

--- src/app/ui/landing-container/landing-container-body/landing-container-body.component.ts
+++ src/app/ui/landing-container/landing-container-body/landing-container-body.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs';
+import { CampaignService } from 'src/app/services/campaign.service';
+import { User } from 'src/app/shared/models/user';
 
 @Component({
   selector: 'crc-landing-container-body',
@@ -12,7 +15,47 @@ export class LandingContainerBodyComponent implements OnInit {
   // disable wheel button
   isDisabled = true;
 
-  constructor() {}
+  // progress bar levels and progress amount
+  progressData: any = {
+    levels: [],
+    amount: 0,
+  };
 
-  ngOnInit(): void {}
+  userData = {
+    unlockedLevel: 0,
+    used: 0,
+  };
+
+  constructor(private campaignService: CampaignService) {}
+
+  ngOnInit(): void {
+    this.getData();
+  }
+
+  getData() {
+    return this.campaignService
+      .getUserData('ufo-double-wheel-190822')
+      .pipe(
+        map((res) => {
+          res.data.state.steps.forEach((step: any) => {
+            step.imageState = 'off';
+            step.points < 5 ? (step.points = 1) : (step.points = 5);
+          });
+          return res.data;
+        })
+      )
+      .subscribe((res: User) => {
+        console.log(res);
+
+        this.progressData = {
+          levels: res.state.steps,
+          amount: Math.floor(Math.min(res.state.progress, 2500)), // limit max score
+        };
+
+        this.userData = {
+          unlockedLevel: res.state.currentStepIndex,
+          used: res.state.used,
+        };
+      });
+  }
 }
