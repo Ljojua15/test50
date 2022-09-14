1b4f13a40a7cc1158600b56747cff6e27045b5e4
lgelashvili <lgelashvili@crocobet.com>
2022-09-14 15:35:49 +0400

    commit

--- src/app/ui/landing-container/landing-container-body/wheel/wheel.component.ts
+++ src/app/ui/landing-container/landing-container-body/wheel/wheel.component.ts
@@ -35,9 +35,8 @@ export class WheelComponent implements OnInit {
   // campaign id for get prize
   campaignId = '';
 
-  // popup
+  // popup configuration
   popup = true;
-
   popupContainerStyles = {
     'background-color': '#1b3a28',
   };
