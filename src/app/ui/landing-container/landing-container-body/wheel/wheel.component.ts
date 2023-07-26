29d71e4f3b2e4e33091b0cca923197efded51c4b
gzardiashvili <gzardiashvili@crocobet.com>
2023-07-26 16:40:43 +0400

    update texts, fix bugs

--- src/app/ui/landing-container/landing-container-body/wheel/wheel.component.ts
+++ src/app/ui/landing-container/landing-container-body/wheel/wheel.component.ts
@@ -8,17 +8,25 @@ import {
   ChangeDetectionStrategy,
   Component,
   ElementRef,
+  EventEmitter,
   Input,
   OnInit,
+  Output,
   ViewChild,
 } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { map, Observable, tap } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
 import { BackdropService } from 'src/app/services/backdrop.service';
 import { CampaignService } from 'src/app/services/campaign.service';
 import { Prize } from 'src/app/shared/models/prize';
 import { environment } from 'src/environments/environment';
+import { checkDate } from './entities';
 
+export enum PrizeType {
+  PLINKO = 'PLINKO_MAX_WHEEL',
+  EGT = 'EGT_INTERACTIVE',
+}
 @Component({
   selector: 'crc-wheel',
   templateUrl: './wheel.component.html',
@@ -27,7 +35,18 @@ import { environment } from 'src/environments/environment';
 })
 export class WheelComponent implements OnInit {
   @ViewChild('prizeElement') prizeElement!: ElementRef;
+  @Input() available = 0;
+  @Output() availableChange = new EventEmitter<boolean>();
   @Input() isDisabled = false;
+  @Input() set isAuthorized(value: boolean) {
+    if (!value) {
+      this.history = [];
+      this.historyPopup = false;
+    } else {
+      this.getHistory();
+    }
+  }
+  history: any = [];
 
   filePath = environment.filePath;
   imagePath = `./../../../../${this.filePath}assets/images`;
@@ -36,7 +55,7 @@ export class WheelComponent implements OnInit {
   arrowImage = `${this.imagePath}/wheel-arrow.webp`;
   frameImage = `${this.imagePath}/wheel-frame.webp`;
   buttonImage = `${this.imagePath}/wheel-btn-active.webp`;
-  buttonDisabledImage = `${this.imagePath}/wheel-btn-disabled.webp`;
+  buttonDisabledImage = `${this.imagePath}/wheel-btn-active.webp`;
   // prizesImage = this.translateService.onLangChange.pipe(
   //   map((lang) => `${this.imagePath}/wheel-prizes-${lang.lang}.webp`)
   // );
@@ -53,9 +72,13 @@ export class WheelComponent implements OnInit {
     { k: 'ეგტ', v: 'asd' },
     { k: 'ეგტ', v: 'asd' },
   ];
+  isLoggedIn!: boolean;
+
   // popup configuration
   infoPopup = false;
   historyPopup = false;
+  winPopup = false;
+  plinko = true;
   popupContainerStyles = {
     'background-color': '#1b3a28',
     'background-image': 'url("./assets/images/info-popup.webp")',
@@ -66,27 +89,37 @@ export class WheelComponent implements OnInit {
     height: '360px',
   };
 
+  disableSpin = false;
+
   wheelDegree = 0;
   offset = 0;
+
+  prizeType: string = PrizeType.PLINKO;
 
   backdrop$: Observable<boolean> | null = null;
   openInfo() {
     this.infoPopup = true;
   }
   openHistory() {
-    this.historyPopup = true;
+    if (this.isLoggedIn) {
+      this.historyPopup = true;
+    }
   }
   constructor(
     private builder: AnimationBuilder,
     private translateService: TranslateService,
     private campaignService: CampaignService,
-    private backdropService: BackdropService
+    private backdropService: BackdropService,
+    private authService: AuthService
   ) {
     this.translateService.onLangChange.subscribe((lang) => {
       console.log(lang.lang);
     });
+    this.authService.isAuthorized().subscribe((res) => {
+      this.isLoggedIn = res;
+    });
   }
-
+  spinAmount: number = 0;
   ngOnInit(): void {
     this.backdrop$ = this.backdropService.backDrop$.asObservable();
   }
@@ -94,17 +127,45 @@ export class WheelComponent implements OnInit {
   closePopup() {
     this.infoPopup = false;
     this.historyPopup = false;
+    this.winPopup = false;
   }
 
+  // getPrize() {
+  //   return this.campaignService
+  //     .getPrize(this.campaignId)
+  //     .pipe(map((res: any) => res.data))
+  //     .subscribe((res: Prize) => this.makeAnimation(res.prizeId));
+  // }
   getPrize() {
+    this.disableSpin = true;
     return this.campaignService
       .getPrize(this.campaignId)
       .pipe(map((res: any) => res.data))
-      .subscribe((res: Prize) => this.makeAnimation(res.prizeId));
+      .subscribe((res: Prize) => {
+        this.spinAmount = res.prize.amount;
+        this.prizeType = res.prize.type;
+        this.makeAnimation(res.prizeId);
+      });
   }
   // getPrize() {
   //   this.makeAnimation(1);
   // }
+
+  getHistory() {
+    this.history = [];
+    this.campaignService.getHistory(this.campaignId).subscribe((res: any) => {
+      res.data.forEach((item: any) => {
+        if (checkDate(item)) {
+          const split1 = item.createdAt.split('T')[0];
+          const [year, month, day] = split1.split('-');
+          item.createdAt = `${month}.${day}`;
+          item.prize = item.prize.name.replace(/-/g, ' ', '-').toUpperCase();
+          this.history.push(item);
+        }
+      });
+    });
+  }
+
   switchPrize(id: number) {
     switch (id) {
       case 1:
@@ -159,8 +220,11 @@ export class WheelComponent implements OnInit {
     const player = myAnimation.create(this.prizeElement.nativeElement);
     player.play();
     player.onDone(() => {
-      // this.openPopup = true;
       // this.isDisabled = false;
+      this.disableSpin = false;
+      this.winPopup = true;
+      this.getHistory();
+      this.availableChange.emit(true);
     });
   }
 }
