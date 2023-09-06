0a5c36405e7350449840cf01c422640d3cb76281
lgelashvili <lgelashvili@crocobet.com>
2023-09-06 14:49:10 +0400

    create game swiper

---
+++ src/app/ui/landing-container/landing-container-body/game-swiper/game-swiper.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'crc-game-swiper',
+  templateUrl: './game-swiper.component.html',
+  styleUrls: ['./game-swiper.component.scss'],
+})
+export class GameSwiperComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
