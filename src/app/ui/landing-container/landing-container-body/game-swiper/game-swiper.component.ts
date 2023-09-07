3e2662d303337e280985ab34d41f996651b9e55e
lgelashvili <lgelashvili@crocobet.com>
2023-09-07 15:12:34 +0400

    create game swiper

--- src/app/ui/landing-container/landing-container-body/game-swiper/game-swiper.component.ts
+++ src/app/ui/landing-container/landing-container-body/game-swiper/game-swiper.component.ts
@@ -1,12 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+type Cards = Array<{ id: number; position: 'active' | 'left' | 'right' }>;
 
 @Component({
   selector: 'crc-game-swiper',
   templateUrl: './game-swiper.component.html',
   styleUrls: ['./game-swiper.component.scss'],
 })
-export class GameSwiperComponent implements OnInit {
-  constructor() {}
+export class GameSwiperComponent {
+  cards: Cards = [
+    { id: 0, position: 'active' },
+    { id: 1, position: 'right' },
+    { id: 2, position: 'left' },
+  ];
 
-  ngOnInit() {}
+  selected = 0;
+
+  onArrowClick(arrow: 'left' | 'right') {
+    if (arrow === 'left') {
+      this.selected--;
+
+      if (this.selected < 0) {
+        this.selected = this.cards.length - 1;
+      }
+    } else {
+      this.selected++;
+      if (this.selected > this.cards.length - 1) {
+        this.selected = 0;
+      }
+    }
+
+    this.cards.forEach((item) => {
+      if (item.id === this.selected) {
+        item.position = 'active';
+      }
+
+      if (this.selected - 1 < 0) {
+        this.cards[2].position = 'left';
+      } else if (item.id === this.selected - 1) {
+        item.position = 'left';
+      }
+
+      if (this.selected + 1 > this.cards.length - 1) {
+        this.cards[0].position = 'right';
+      } else if (item.id === this.selected + 1) {
+        item.position = 'right';
+      }
+    });
+  }
 }
