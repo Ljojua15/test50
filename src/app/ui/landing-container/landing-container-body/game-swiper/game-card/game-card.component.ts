2bde2113fee89a23bb52f02b9a01c8b89128d8d7
lgelashvili <lgelashvili@crocobet.com>
2023-09-21 16:50:57 +0400

    add video for card

--- src/app/ui/landing-container/landing-container-body/game-swiper/game-card/game-card.component.ts
+++ src/app/ui/landing-container/landing-container-body/game-swiper/game-card/game-card.component.ts
@@ -1,10 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, Component, Input, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'crc-game-card',
   templateUrl: './game-card.component.html',
   styleUrls: ['./game-card.component.scss'],
 })
-export class GameCardComponent {
+export class GameCardComponent implements AfterViewInit {
+  // @ViewChild('vid') video: any;
+  @Input() video: string;
   @Input() name: string;
+
+  ngAfterViewInit(): void {
+    // this.video.play();
+    // @ts-ignore: Unreachable code error
+    // document?.getElementById('vid')?.play();
+  }
 }
